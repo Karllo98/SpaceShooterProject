e11d741ffcc21885624ebc38468ded4765da3c40
Karllo98 <kaaaroll1998@gmail.com>
2021-01-17 20:48:29 +0100

    Added simple GameScreen class

---
+++ core/src/pl/projekt/spaceproject/windows/GameScreen.java
@@ -0,0 +1,14 @@
+package pl.projekt.spaceproject.windows;
+
+import pl.projekt.spaceproject.SpaceGame;
+
+public class GameScreen extends ParentScreen{
+    
+    public GameScreen(SpaceGame game) {
+        super(game);
+        initialize();
+    }
+
+    private void initialize() {
+    }
+}
