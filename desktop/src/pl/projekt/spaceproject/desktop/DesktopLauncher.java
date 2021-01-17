fd99756ebfa1dd68986f80d39e7985b128b3b4b5
Karllo98 <kaaaroll1998@gmail.com>
2021-01-17 17:40:41 +0100

    init

---
+++ desktop/src/pl/projekt/spaceproject/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.projekt.spaceproject.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.projekt.spaceproject.SpaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceGame(), config);
+	}
+}
