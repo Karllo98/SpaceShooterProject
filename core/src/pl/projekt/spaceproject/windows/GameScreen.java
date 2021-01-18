7386c7993980a94c07afe52d7f9b84c102800582
Karllo98 <kaaaroll1998@gmail.com>
2021-01-18 01:06:45 +0100

    Added background image

--- core/src/pl/projekt/spaceproject/windows/GameScreen.java
+++ core/src/pl/projekt/spaceproject/windows/GameScreen.java
@@ -1,11 +1,14 @@
 package pl.projekt.spaceproject.windows;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import pl.projekt.spaceproject.SpaceGame;
 import pl.projekt.spaceproject.gamecomponents.SpaceShip;
 
 public class GameScreen extends ParentScreen {
 
     private SpaceShip ship;
+    private Image backgroundImage;
 
     public GameScreen(SpaceGame game) {
         super(game);
@@ -13,7 +16,13 @@ public class GameScreen extends ParentScreen {
     }
 
     private void initialize() {
+        initBackground();
         initShip();
+    }
+
+    private void initBackground() {
+        backgroundImage = new Image(new Texture("background.jpg"));
+        stage.addActor(backgroundImage);
     }
 
     private void initShip() {
